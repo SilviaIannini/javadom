a849394ae37f53f9d72f68ab1233208518ec8a32
SilviaIannini <153084625+SilviaIannini@users.noreply.github.com>
2024-02-16 15:11:28 +0100

    Add files via upload

---
+++ javadom/js.js
@@ -0,0 +1,8 @@
+const padre = document.querySelector("ul").parentNode;
+console.log(padre)
+const secondoEl=document.querySelector("ul").children[1];
+consolelog(secondoEl)
+const nextSibiling= secondChild.nextElementsSibiling;
+console.log(nextSibiling)
+const previousSibiling= secondChild.previousElementsSibiling;
+console.log(previousSibiling)
